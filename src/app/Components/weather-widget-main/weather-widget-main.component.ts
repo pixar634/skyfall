3c086b553d533863139cb8214d26f323403f135b
Soumava Paul <soumava@mdbrd.in>
2020-08-08 19:41:44 +0530

    changed fonts

---
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget-main',
+  templateUrl: './weather-widget-main.component.html',
+  styleUrls: ['./weather-widget-main.component.css']
+})
+export class WeatherWidgetMainComponent implements OnInit {
+  WeatherData: any;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getWeatherData();
+    console.log(this.WeatherData);
+
+  }
+
+  getWeatherData() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=7392400f17141bdfa19378928900ebdb')
+      .then(response => response.json())
+      .then(data => { this.setWeatherData(data); })
+  }
+  setWeatherData(data) {
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleDateString();
+    let currentDate = new Date();
+    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
+    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherWidgetMainComponent } from './Components/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetMainComponent
   ],
   imports: [
     BrowserModule,
