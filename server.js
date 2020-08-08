72e701f89a455ac38e025b3d4c09cdc5cf8d77f1
Soumava Paul <soumava@mdbrd.in>
2020-08-08 19:56:46 +0530

    updated server info

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/weather'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/myapp/index.html'));
+        '/dist/weather/index.html'));
 });
 app.listen(process.env.PORT || 8080);
