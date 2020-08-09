813858ccc6a0e76a20afa1d007e753c54badd782
Soumava Paul <soumava@mdbrd.in>
2020-08-10 03:08:31 +0530

    add cities functionality:in progress

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const path = require('path');
+//Install express server
+const express = require("express");
+const path = require("path");
+
 const app = express();
-app.use(express.static(__dirname + '/dist/weather'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/weather/index.html'));
+
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + "/dist/weather"));
+
+app.get("*", function (req, res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + "/dist/weather/index.html"));
 });
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
